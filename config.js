7258bbf133b9a303cdc646c47bd8f5c41f9e4ff6
Aditya Verma <av392172@gmail.com>
2020-11-28 06:31:45 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     "secretKey": "12345-67890-09876-54321",
-    "mongoUrl": "mongodb://localhost:27017/conFusion"
+    "mongoUrl": "mongodb://localhost:27017/conFusion",
+    'facebook': {
+        clientId: '1325312487833455',
+        clientSecret: '17cb3354d9a0dcd57780ec9f4154b582'
+    }
 }
